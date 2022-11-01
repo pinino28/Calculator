0f403cf69145e02d10e2faa0788ffd669122e455
= <=>
2022-11-01 13:20:55 -0300

    calculator code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+import { Helmet } from 'react-helmet';
+import ClockLogic from './Clock25+5/Clock';
 import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      <ClockLogic />
+     
     </div>
   );
 }
